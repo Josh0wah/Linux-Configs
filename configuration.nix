ca1a0b84c84e2dda5beb289be7011b8e3e04e642
Joshua Singleton <134548428+Josh0wah@users.noreply.github.com>
2025-10-03 01:59:29 -0500

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -60,12 +60,13 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -142,6 +143,7 @@
     swaybg
     hyprshot
     xournalpp
+    guitarix
     # spotify via flatpak install
   ];
 
