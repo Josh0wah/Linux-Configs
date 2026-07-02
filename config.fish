6d2f13308fd54a6896124fc66cbfcd56ecdb5754
Joshua Singleton <134548428+Josh0wah@users.noreply.github.com>
2026-07-02 12:45:51 -0500

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+alias music="cd git/low-tide/ && source .venv/bin/activate.fish && low-tide"
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
